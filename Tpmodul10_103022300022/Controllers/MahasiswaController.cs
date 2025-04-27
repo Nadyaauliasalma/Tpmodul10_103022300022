a169c83ef6edfda4edc1bfcf0bd3bc5da88edc18
Nadya Aulia <nadyaauliasalma622@gmail.com>
2025-04-27 16:10:18 +0700

    menambahkan API Mahasiwa

---
+++ Tpmodul10_103022300022/Controllers/MahasiswaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tpmodul10_103022300022.Controllers
+{
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Nadya Aulia Salma", "103022300022"),
+            new Mahasiswa("Deswita Syaharani", "103022300004"),
+            new Mahasiswa("Apriliani", "103022300054"),
+            new Mahasiswa("Aisyah", "103022300114"),
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            dataMahasiswa.Add(mahasiswa);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ Tpmodul10_103022300022/Mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿namespace Tpmodul10_103022300022
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public Mahasiswa(string Nama, string Nim)
+        {
+            this.Nama = Nama;
+            this.Nim = Nim;
+        }
+    }
+}
